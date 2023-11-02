73d6ef288afa80d7a6319dbce054df6cad7a1aca
Aksellofstedtnielsen <aksel98@outlook.com>
2023-11-02 11:44:18 +0100

    Working channel + tableu

--- radio_application_flutter/lib/main.dart
+++ radio_application_flutter/lib/main.dart
@@ -1,51 +1,83 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:radio_application_flutter/screens/home.dart';
 import 'package:radio_application_flutter/tableu.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: RadioApp(),
+      home: Home(),
     );
   }
 }
 
-class RadioApp extends StatefulWidget {
-  @override
-  _RadioAppState createState() => _RadioAppState();
-}
-
-class _RadioAppState extends State<RadioApp> {
-  //Variables for api data and programs blabla
-
+class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Radio App'),
-        ),
-        body: Home());
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      body: FutureBuilder<List<Map<String, dynamic>?>?>(
+        future: fetchChannelsApi(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (snapshot.hasError) {
+            return Text('Error: ${snapshot.error}');
+          } else {
+            List<Map<String, dynamic>?> channels = snapshot.data ?? [];
+
+            return ListView.builder(
+              itemCount: channels.length,
+              itemBuilder: (context, index) {
+                final item = channels[index]!;
+                int channelId = item['id'];
+                return GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) =>
+                            TableuWidget(channelId: channelId),
+                      ),
+                    );
+                  },
+                  child: ListTile(
+                    leading: Image.network(item['image']),
+                    title: Text(item['name']),
+                    subtitle: Text(item['tagline']),
+                  ),
+                );
+              },
+            );
+          }
+        },
+      ),
+    );
   }
-}
 
-void _showProgramDetails(BuildContext context, int index) {
-  //Show detailed stuff
-
-  showDialog(
-      context: context,
-      builder: (context) {
-        return const AlertDialog(
-          title: Text('Title'),
-          content: Column(
-            children: [
-              Text('Description')
-              //More stuff
-            ],
-          ),
-        );
-      });
+  Future<List<Map<String, dynamic>?>?> fetchChannelsApi() async {
+    final dio = Dio();
+    try {
+      final response =
+          await dio.get('http://api.sr.se/api/v2/channels?format=json');
+      if (response.statusCode == 200) {
+        final data = response.data['channels'];
+        List<Map<String, dynamic>?> channels = List<Map<String, dynamic>?>.from(
+            data.map((item) => item as Map<String, dynamic>?));
+        return channels;
+      } else {
+        throw Exception('Failed to load channels from API');
+      }
+    } catch (e) {
+      throw Exception('Failed to load channels from API: $e');
+    }
+  }
 }
--- radio_application_flutter/lib/tableu.dart
+++ radio_application_flutter/lib/tableu.dart
@@ -2,41 +2,52 @@ import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
 class TableuWidget extends StatelessWidget {
+  final int channelId;
+
+  const TableuWidget({Key? key, required this.channelId}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<List<Map<String, dynamic>?>?>(
-      future: fetchTableuApi(),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return const Center(
-            child: CircularProgressIndicator(),
-          );
-        } else if (snapshot.hasError) {
-          return Text('Error: ${snapshot.error}');
-        } else {
-          List<Map<String, dynamic>?> tableu = snapshot.data ?? [];
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Tableu Widget'),
+      ),
+      body: FutureBuilder<List<Map<String, dynamic>?>?>(
+        future: fetchTableuApi(channelId),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (snapshot.hasError) {
+            return Text('Error: ${snapshot.error}');
+          } else {
+            List<Map<String, dynamic>?> tableu = snapshot.data ?? [];
 
-          return ListView.builder(
-            itemCount: tableu.length,
-            itemBuilder: (context, index) {
-              final item = tableu[index]!;
-              return ListTile(
-                leading: Image.network(item['imageurl']),
-                title: Text(item['title']),
-                subtitle: Text(item['description']),
-              );
-            },
-          );
-        }
-      },
+            return ListView.builder(
+              itemCount: tableu.length,
+              itemBuilder: (context, index) {
+                final item = tableu[index]!;
+                return ListTile(
+                  leading: Image.network(
+                      item['imageurl']), // Check the field name from your API
+                  title: Text(item['title']),
+                  subtitle: Text(item['description']),
+                );
+              },
+            );
+          }
+        },
+      ),
     );
   }
 
-  Future<List<Map<String, dynamic>?>?> fetchTableuApi() async {
+  Future<List<Map<String, dynamic>?>?> fetchTableuApi(int channelId) async {
+    print("channel id: $channelId");
     final dio = Dio();
     try {
       final response = await dio.get(
-          'https://api.sr.se/api/v2/scheduledepisodes?channelid=164&format=json');
+          'http://api.sr.se/api/v2/scheduledepisodes?channelid=${channelId.toString()}&format=json');
       if (response.statusCode == 200) {
         final data = response.data['schedule'];
         List<Map<String, dynamic>?> tableu = List<Map<String, dynamic>?>.from(
